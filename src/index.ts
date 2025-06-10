7127195b4bef2489ac28cbeabc6ceafdeac5a8c7
x4132 <31595285+x4132@users.noreply.github.com>
2025-06-09 17:42:15 -0700

    fix: remove uncessary CORS

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
 import { Hono } from 'hono'
 import { cors } from 'hono/cors'
-import { auth } from './lib/auth'; 
+import { auth } from './lib/auth';
 import { betterAuth } from 'better-auth'
 
 const app = new Hono()
+
 
 app.get('/', (c) => {
   return c.text('nyanya')
 })
 
-app.use('*', cors({ origin: 'http://localhost:3001', credentials: true }));
 app.on(["POST", "GET"], "/api/auth/**", (c) => auth.handler(c.req.raw));
 
 export default app
